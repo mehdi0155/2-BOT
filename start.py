1f1ff5115fceb5ffc314e2078899f7b4e0f63172
mehdi0155 <newme0155@gmail.com>
2025-04-29 01:31:20 +0330

    Create start.py

---
+++ start.py
@@ -0,0 +1,13 @@
+from uploader_bot.main import server as uploader_server
+from checker_bot.main import server as checker_server
+import threading
+
+def run_uploader():
+    uploader_server.run(host="0.0.0.0", port=5000)
+
+def run_checker():
+    checker_server.run(host="0.0.0.0", port=5001)
+
+if __name__ == "__main__":
+    threading.Thread(target=run_uploader).start()
+    threading.Thread(target=run_checker).start()

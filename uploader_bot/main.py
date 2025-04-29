05e4e080dc025abe26d42980579f5f3f53f4ebc8
mehdi0155 <newme0155@gmail.com>
2025-04-29 19:04:57 +0330

    Update main.py

--- uploader_bot/main.py
+++ uploader_bot/main.py
@@ -105,8 +105,9 @@ def admin_panel(message):
 @bot.message_handler(func=lambda m: is_admin(m.from_user.id) and m.text == "📂 آپلود ویدیو")
 def ask_video(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-markup.add("برگشت")
-msg = bot.send_message(message.chat.id, "لطفاً ویدیو را ارسال کنید.", reply_markup=markup)
+    markup.add("برگشت")
+    msg = bot.send_message(message.chat.id, "لطفاً ویدیو را ارسال کنید.", reply_markup=markup)
+    bot.register_next_step_handler(msg, receive_video)
 
 
 def receive_video(message):

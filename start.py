a304c3723aee2df2bf8aba24d02a387d401cf3ea
mehdi0155 <newme0155@gmail.com>
2025-04-29 10:37:02 +0330

    Update start.py

--- start.py
+++ start.py
@@ -1,13 +1,21 @@
-from uploader_bot.main import server as uploader_server
-from checker_bot.main import server as checker_server
+from flask import Flask
 import threading
+import time
 
-def run_uploader():
-    uploader_server.run(host="0.0.0.0", port=5000)
+from uploader_bot.main import setup_routes as uploader_routes
+from checker_bot.main import setup_routes as checker_routes
 
-def run_checker():
-    checker_server.run(host="0.0.0.0", port=5001)
+server = Flask(__name__)
+
+uploader_routes(server)
+checker_routes(server)
+
+# جلوگیری از خواب رفتن Render
+def keep_alive():
+    while True:
+        time.sleep(15 * 60)  # هر ۱۵ دقیقه
+        print("Keeping alive...")
 
 if __name__ == "__main__":
-    threading.Thread(target=run_uploader).start()
-    threading.Thread(target=run_checker).start()
+    threading.Thread(target=keep_alive).start()
+    server.run(host="0.0.0.0", port=10000)

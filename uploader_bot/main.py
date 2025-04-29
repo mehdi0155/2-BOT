656686b640c8b8b1c1990548d35680bb03575092
mehdi0155 <newme0155@gmail.com>
2025-04-29 11:24:55 +0330

    Update main.py

--- uploader_bot/main.py
+++ uploader_bot/main.py
@@ -4,6 +4,8 @@ import random
 import string
 import os
 import json
+import threading
+import time
 
 TOKEN = "7920918778:AAFF4MDkYX4qBpuyXyBgcuCssLa6vjmTN1c"
 CHANNEL = "@hottof"
@@ -16,6 +18,7 @@ pending_posts = {}
 
 DB_FILE = "db.json"
 
+
 def save_to_db(link_id, file_unique_id):
     db = {}
     if os.path.exists(DB_FILE):
@@ -25,8 +28,10 @@ def save_to_db(link_id, file_unique_id):
     with open(DB_FILE, "w") as f:
         json.dump(db, f)
 
+
 def is_admin(user_id):
     return user_id in ADMINS
+
 
 def generate_link_id():
     while True:
@@ -38,31 +43,33 @@ def generate_link_id():
         if link_id not in db:
             return link_id
 
-@bot.message_handler(commands=['start'])
+
+@bot.message_handler(commands=['start', 'panel'])
 def start(message):
     if is_admin(message.from_user.id):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        markup.add(types.KeyboardButton("آپلود ویدیو"))
-        bot.send_message(message.chat.id, "خوش آمدید.", reply_markup=markup)
+        markup.add("آپلود ویدیو", "مدیریت عضویت")
+        bot.send_message(message.chat.id, "به پنل مدیریت خوش آمدید.", reply_markup=markup)
     else:
-        bot.send_message(message.chat.id, "دسترسی ندارید.")
+        bot.send_message(message.chat.id, "برای دریافت فایل، لینک را از کانال انتخاب کنید.")
+
 
 @bot.message_handler(func=lambda m: is_admin(m.from_user.id) and m.text == "آپلود ویدیو")
 def ask_video(message):
     msg = bot.send_message(message.chat.id, "ویدیو را ارسال کنید.")
     bot.register_next_step_handler(msg, receive_video)
 
+
 def receive_video(message):
     if not message.video:
         bot.send_message(message.chat.id, "فقط ویدیو بفرستید.")
         return
-    user_data[message.from_user.id] = {
-        'file_unique_id': message.video.file_unique_id
-    }
+    user_data[message.from_user.id] = {'file_unique_id': message.video.file_unique_id}
     markup = types.InlineKeyboardMarkup()
     markup.add(types.InlineKeyboardButton("ندارم", callback_data="no_cover"))
     bot.send_message(message.chat.id, "کاور را ارسال کنید یا روی 'ندارم' کلیک کنید.", reply_markup=markup)
     bot.register_next_step_handler(message, receive_cover)
+
 
 @bot.callback_query_handler(func=lambda call: call.data == "no_cover")
 def no_cover(call):
@@ -72,6 +79,7 @@ def no_cover(call):
         data['cover'] = None
         msg = bot.send_message(call.message.chat.id, "کپشن و توضیح فایل را بفرستید.")
         bot.register_next_step_handler(msg, receive_caption)
+
 
 def receive_cover(message):
     if message.photo:
@@ -84,11 +92,13 @@ def receive_cover(message):
     else:
         bot.send_message(message.chat.id, "فقط عکس بفرست یا روی 'ندارم' کلیک کن.")
 
+
 def receive_caption(message):
     data = user_data.get(message.from_user.id)
     if data:
         data['caption'] = message.text
         preview_post(message)
+
 
 def preview_post(message):
     data = user_data.get(message.from_user.id)
@@ -115,6 +125,7 @@ def preview_post(message):
         )
         bot.send_message(message.chat.id, "آیا این پست ارسال شود؟", reply_markup=confirm_markup)
 
+
 @bot.callback_query_handler(func=lambda call: call.data in ["send_now", "cancel_post"])
 def process_confirmation(call):
     bot.answer_callback_query(call.id)
@@ -131,9 +142,9 @@ def process_confirmation(call):
             markup.add(types.InlineKeyboardButton("مشاهده فایل", url=link))
 
             if data.get('cover'):
-                bot.send_photo(CHANNEL, data['cover'], caption=caption, reply_markup=markup)
+                msg = bot.send_photo(CHANNEL, data['cover'], caption=caption, reply_markup=markup)
             else:
-                bot.send_message(CHANNEL, caption, reply_markup=markup)
+                msg = bot.send_message(CHANNEL, caption, reply_markup=markup)
 
             bot.send_message(call.message.chat.id, "پست با موفقیت ارسال شد.")
             user_data.pop(user_id, None)
@@ -144,8 +155,38 @@ def process_confirmation(call):
         pending_posts.pop(user_id, None)
         bot.send_message(call.message.chat.id, "ارسال لغو شد.")
 
+
+@bot.message_handler(commands=['start'])
+def handle_user_start(message):
+    args = message.text.split()
+    if len(args) > 1:
+        link_id = args[1]
+        file_unique_id = load_file_id(link_id)
+        if file_unique_id:
+            msg = bot.send_message(message.chat.id, "این ویدیو تا ۱۵ ثانیه دیگر حذف خواهد شد.")
+            vid_msg = bot.send_video(message.chat.id, file_unique_id, caption="@hottof | تُفِ داغ")
+
+            threading.Thread(target=delete_after_delay, args=(message.chat.id, vid_msg.message_id, msg.message_id)).start()
+
+
+def load_file_id(link_id):
+    if not os.path.exists(DB_FILE):
+        return None
+    with open(DB_FILE, "r") as f:
+        db = json.load(f)
+    return db.get(link_id)
+
+
+def delete_after_delay(chat_id, video_msg_id, warn_msg_id):
+    time.sleep(15)
+    try:
+        bot.delete_message(chat_id, video_msg_id)
+        bot.delete_message(chat_id, warn_msg_id)
+    except:
+        pass
+
+
 def setup_routes(server):
-    import flask
     @server.route('/uploader/' + TOKEN, methods=['POST'])
     def get_uploader_message():
         bot.process_new_updates([

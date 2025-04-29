0d44e8f6e1130755d43c6540ee0492543ac63c01
mehdi0155 <newme0155@gmail.com>
2025-04-29 10:49:05 +0330

    Update start.py

--- start.py
+++ start.py
@@ -2,18 +2,25 @@ from flask import Flask
 import threading
 import time
 
-from uploader_bot.main import setup_routes as uploader_routes
-from checker_bot.main import setup_routes as checker_routes
+from uploader_bot.main import setup_routes as uploader_routes, bot as uploader_bot
+from checker_bot.main import setup_routes as checker_routes, bot as checker_bot, TOKEN as CHECKER_TOKEN
 
 server = Flask(__name__)
 
 uploader_routes(server)
 checker_routes(server)
 
+# تنظیم webhook برای هر دو ربات
+uploader_bot.remove_webhook()
+checker_bot.remove_webhook()
+
+uploader_bot.set_webhook(url="https://two-bot-hcxp.onrender.com/uploader/" + uploader_bot.token)
+checker_bot.set_webhook(url="https://two-bot-hcxp.onrender.com/checker/" + CHECKER_TOKEN)
+
 # جلوگیری از خواب رفتن Render
 def keep_alive():
     while True:
-        time.sleep(15 * 60)  # هر ۱۵ دقیقه
+        time.sleep(15 * 60)
         print("Keeping alive...")
 
 if __name__ == "__main__":
